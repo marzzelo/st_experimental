2d22ddd58567821fb71eeb9907722548d0000148
marzzelo <zedlavolecram@gmail.com>
2024-04-22 13:33:20 -0300

    updated FTC calc

--- pages/Calculadora_FTC.py
+++ pages/Calculadora_FTC.py
@@ -4,7 +4,7 @@ import streamlit as st
 from config import page_config
 
 
-def compute_end_time(i_datetime, time_to_add):
+def compute_end_time(i_datetime: pd.Timestamp, time_to_add: float) -> tuple[pd.Timestamp, str, pd.Timestamp]:
     """
     Compute the end time of an event given the start time and the duration of the event.
     :param i_datetime:
@@ -24,17 +24,13 @@ class FTCCalc:
     def __init__(self):
         # use a frequency icon
         page_config(title="Calculadora de Frecuencia", icon=":infinity:")
-
-    def run(self):
-        self.show_form()
-
-        self.calculate(st.session_state.var_to_calc)
-
-        if 'result' in st.session_state:
-            self.show_result()
-
-    @staticmethod
-    def show_form():
+        # if 'result' not in st.session_state:
+        #     st.session_state.result_freq = 1.0
+        #     st.session_state.result_time = 1.0
+        #     st.session_state.result_cycles = 1
+        #     st.session_state.result_icycles = 0
+        result = ""
+        units = ""
 
         with st.form(key='form1'):
             st.subheader("Calculadora de Frecuencia, Ciclos, Tiempo")
@@ -45,48 +41,42 @@ class FTCCalc:
             with col1:
                 freq = st.number_input(label="Frecuencia [Hz]", min_value=0.0, value=1.0, key='input_freq')
                 time = st.number_input(label="Tiempo [s]", min_value=0.0, value=1.0, key='input_time')
+                idate = st.date_input(label="Fecha de inicio", key='input_idate')
 
             with col2:
                 cycles = st.number_input(label="Ciclos [#]", min_value=0, value=1, key='input_cycles')
                 icycles = st.number_input(label="Ciclos Iniciales [#]", min_value=0, value=0, key='input_icycles')
+                itime = st.time_input(label="Hora de inicio", key='input_itime')
 
             var_to_calc = st.radio("Seleccione la variable a calcular:", ["Frecuencia", "Tiempo", "Ciclos", "Ciclos Iniciales"],
                                    key='var_to_calc', index=1)
 
-            st.form_submit_button("Calcular")
+            if st.form_submit_button("Calcular"):
 
-    @staticmethod
-    def calculate(var_to_calc):
-        """
-        Calculate the selected variable
-        cycles = freq * time + icycles
-        """
-        result = 0
-        units = ""
-        if var_to_calc == "Frecuencia":
-            units = "Hz"
-            result = (st.session_state.input_cycles - st.session_state.input_icycles) / st.session_state.input_time
+                if var_to_calc == "Frecuencia":
+                    units = "Hz"
+                    result = (cycles - icycles) / time
 
-        elif var_to_calc == "Tiempo":
-            units = "s"
-            result = (st.session_state.input_cycles - st.session_state.input_icycles) / st.session_state.input_freq
+                elif var_to_calc == "Tiempo":
+                    units = ""
+                    result0 = (cycles - icycles) / freq
+                    fdate, dow, ftime = compute_end_time(pd.to_datetime(str(idate) + ' ' + str(itime)), result0 / 3600)
+                    # format fdate as 'dd/mm/yyyy'
+                    fdatef = fdate.strftime('%d-%m-%Y')
+                    result = f"{result0} s<br>{dow} {fdatef}, {ftime}"
 
-        elif var_to_calc == "Ciclos":
-            units = "ciclos"
-            result = st.session_state.input_freq * st.session_state.input_time + st.session_state.input_icycles
+                elif var_to_calc == "Ciclos":
+                    units = "ciclos"
+                    result = freq * time + icycles
 
-        elif var_to_calc == "Ciclos Iniciales":
-            units = "ciclos"
-            result = st.session_state.input_freq * st.session_state.input_time - st.session_state.input_cycles
+                elif var_to_calc == "Ciclos Iniciales":
+                    units = "ciclos"
+                    result = freq * time - cycles
 
-        st.session_state.result = result
-        st.session_state.units = units
-
-    @staticmethod
-    def show_result():
-        st.markdown(f"<p>Resultado:</p><h2 style='color: magenta;'>{st.session_state.result} {st.session_state.units}</h4>", unsafe_allow_html=True)
+        if result:
+            st.markdown("Resultado:<br><span style='color: magenta; font-size: 24px;'>"
+                        f"{var_to_calc} = {result} {units}</span>", unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
     app = FTCCalc()
-    app.run()
